141a0dccebc05773979fc25c391dd4b04142c3c0
Muhammad Anis <anisiconic@gmail.com>
2017-02-16 15:58:22 +0700

    fix api for visitor with new schema

--- django_project/pycon/api_views/visitors.py
+++ django_project/pycon/api_views/visitors.py
@@ -13,7 +13,7 @@ class VisitorsApi(APIView):
     def get(self, request, conference_id=None):
         if conference_id:
             visitors = Visit.objects.filter(conference=conference_id)
-            serializer = VisitSerializer(visitors)
+            serializer = VisitSerializer(visitors,many=True)
             return Response(serializer.data)
         else:
             return Response([])
