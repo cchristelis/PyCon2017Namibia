4dec37f72ec898f4849b00b82067245653ed0026
Muhammad Anis <anisiconic@gmail.com>
2017-02-16 11:20:19 +0700

    fix changes from django 1.8 to 1.10

--- django_project/core/urls.py
+++ django_project/core/urls.py
@@ -1,17 +1,16 @@
 # coding=utf-8
 """Project level url handler."""
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
+urlpatterns=[
     url(r'^admin/', include(admin.site.urls)),
     url(r'^pycon/', include('pycon.urls', namespace='pycon')),
-)
+]
 
 if settings.DEBUG:
     urlpatterns += static(
