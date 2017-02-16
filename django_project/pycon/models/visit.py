fb88deca69273c8e7bcdb505586a38296813b5da
Muhammad Anis <anisiconic@gmail.com>
2017-02-16 11:29:39 +0700

    not changing db name and updated model

--- django_project/pycon/models/visit.py
+++ django_project/pycon/models/visit.py
@@ -7,4 +7,4 @@ class Visit(models.Model):
     name = models.CharField(max_length=200)
     home = models.PointField(null=True,blank=True)
     date_left = models.DateTimeField()
-    gravatar = models.URLField
+    gravatar = models.URLField(null=True,blank=True)
