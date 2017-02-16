3cfdb2e4400f4cf4c5c8384b86051543d09d5d16
Muhammad Anis <anisiconic@gmail.com>
2017-02-16 15:16:28 +0700

    add logo and location

--- django_project/pycon/models/conference.py
+++ django_project/pycon/models/conference.py
@@ -7,6 +7,7 @@ class Conference(models.Model):
     conference_name = models.CharField(max_length=200)
     conference_code = models.CharField(max_length=200)
     location = models.PointField(null=True, blank=True)
+    logo    = models.ImageField(null=True,blank=True)
 
     def __unicode__(self):
         return '[%s] %s' % (self.conference_code, self.conference_name)
