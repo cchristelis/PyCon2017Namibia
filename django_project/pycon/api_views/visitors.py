f90afc5de062c0454531885c67a91379cf2e3f48
meomancer <meomancer@gmail.com>
2017-02-17 09:20:00 +0700

    clean code

--- django_project/pycon/api_views/visitors.py
+++ django_project/pycon/api_views/visitors.py
@@ -5,15 +5,15 @@ __copyright__ = 'kartoza.com'
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from pycon.models.conference_visit_entry import  Visit
-from pycon.serializers.conference_visit_entry_serializer import VisitSerializer
+from pycon.models.visit import Visit
+from pycon.serializers.visit_serializer import VisitSerializer
 
 
 class VisitorsApi(APIView):
     def get(self, request, conference_id=None):
         if conference_id:
             visitors = Visit.objects.filter(conference=conference_id)
-            serializer = VisitSerializer(visitors,many=True)
+            serializer = VisitSerializer(visitors, many=True)
             return Response(serializer.data)
         else:
             return Response([])
